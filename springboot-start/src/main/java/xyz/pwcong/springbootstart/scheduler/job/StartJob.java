6f68e0edaf0a7ac9d497eb7874bcbdb27a69dc0c
彭伟聪 <pwcong@Pwcong.local>
2018-10-01 21:50:50 +0800

    [*] modify springbootstart

--- springboot-start/src/main/java/xyz/pwcong/springbootstart/scheduler/job/StartJob.java
+++ springboot-start/src/main/java/xyz/pwcong/springbootstart/scheduler/job/StartJob.java
@@ -2,10 +2,14 @@ package xyz.pwcong.springbootstart.scheduler.job;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configurable
+@EnableScheduling
 public class StartJob {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
